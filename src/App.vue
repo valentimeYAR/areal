fb983238046d37f0c08f5b6723e4d17ee25d35e6
Valentin Kosyakov <valentin.kosyakovEnd@yandex.ru>
2023-05-06 18:30:53 +0300

    create header component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 
-
+import './app.scss'
+import Header from "@/components/Header.vue";
 </script>
 
 <template>
-
+  <Header/>
 </template>
 
 <style scoped>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,17 @@
 <template>
-
+  <div class="wrapper">
+      <div class="container">
+          <h1 class="logo">AREAL</h1>
+          <a href="https://github.com/valentimeYAR" class="git">GIT</a>
+      </div>
+  </div>
 </template>
 
 <script>
 export default {
     name: "Header"
 }
+import '../assets/styles/header.scss'
 </script>
 
 <style scoped>
